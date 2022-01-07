1256dfe67c266de1655b77007e81dde3ec230c37
Raffay Syed <raffayhusayn@gmail.com>
2022-01-07 00:34:29 -0500

    Create a servlet and add code to make the embedded tomcat server run

--- src/main/java/com/fclass/App.java
+++ src/main/java/com/fclass/App.java
@@ -1,13 +1,28 @@
 package com.fclass;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws LifecycleException {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+        Context context = tomcat.addContext("",null); // http:localhost:8081/<nothing>/<servlet>
+        //Spring Boot has a dispatcher servlet instead of a HelloServlet which controls the flow to and from all the custom servlets
+        tomcat.addServlet(context, "helloServlet" , new HelloServlet());
+        //mapping http://localhost:8080/ to a servlet named "helloServlet" ---> in this case the Servlet Class is also called HelloServlet but that doesn't have to be the case
+        context.addServletMappingDecoded( "/", "helloServlet");
+
+        tomcat.start();
+        System.out.println("starting tomcat server");
+        new Thread(()->tomcat.getServer().await()).start();
+
+
     }
 }
--- src/main/java/com/fclass/HelloServlet.java
+++ src/main/java/com/fclass/HelloServlet.java
@@ -1,2 +1,17 @@
-package com.fclass;public class HelloServlet {
+package com.fclass;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //prints out the current time in H1
+        resp.getWriter().println("<html><h1> " + LocalDateTime.now() + " </h1></html>");
+    }
 }

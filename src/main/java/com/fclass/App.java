fc13dfa034f9ca61aa33e990ba1e847f133d881a
Raffay Syed <raffayhusayn@gmail.com>
2022-01-07 02:53:06 -0500

    create a configuration Internal Class and refactored the tomcat server lauching logic to a different class

--- src/main/java/com/fclass/App.java
+++ src/main/java/com/fclass/App.java
@@ -3,6 +3,10 @@ package com.fclass;
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Hello world!
@@ -10,19 +14,20 @@ import org.apache.catalina.startup.Tomcat;
  */
 public class App 
 {
+    @Configuration
+    public static class MyAppConfig{
+
+        @Bean
+        public TomcatLaucher getTomcatLauncher(){
+            return new TomcatLaucher();
+        }
+    }
+
     public static void main( String[] args ) throws LifecycleException {
-        Tomcat tomcat = new Tomcat();
-        tomcat.setPort(8080);
-        Context context = tomcat.addContext("",null); // http:localhost:8081/<nothing>/<servlet>
-        //Spring Boot has a dispatcher servlet instead of a HelloServlet which controls the flow to and from all the custom servlets
-        tomcat.addServlet(context, "helloServlet" , new HelloServlet());
-        //mapping http://localhost:8080/ to a servlet named "helloServlet" ---> in this case the Servlet Class is also called HelloServlet but that doesn't have to be the case
-        context.addServletMappingDecoded( "/", "helloServlet");
 
-        tomcat.start();
-        System.out.println("starting tomcat server");
-        new Thread(()->tomcat.getServer().await()).start();
-
+        new AnnotationConfigApplicationContext(MyAppConfig.class);
+//        TomcatLaucher tomcatLaucher = ctx.getBean(TomcatLaucher.class);
+//        tomcatLaucher.launch();
 
     }
 }
--- src/main/java/com/fclass/TomcatLaucher.java
+++ src/main/java/com/fclass/TomcatLaucher.java
@@ -1,2 +1,29 @@
-package com.fclass;public class TomcatLaucher {
+package com.fclass;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+import javax.annotation.PostConstruct;
+
+public class TomcatLaucher {
+
+    @PostConstruct // call this method right after the constructor is called which in this case is called
+    //when the application is started because the Bean's scope by default is Singleton meaning all the
+    //beans are created when the application is started and the same Bean is used throughout the application
+    public void launch() throws LifecycleException {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+        Context context = tomcat.addContext("",null); // http:localhost:8081/<nothing>/<servlet>
+        //Spring Boot has a dispatcher servlet instead of a HelloServlet which controls the flow to and from all the custom servlets
+        tomcat.addServlet(context, "helloServlet" , new HelloServlet());
+        //mapping http://localhost:8080/ to a servlet named "helloServlet" ---> in this case the Servlet Class is also called HelloServlet but that doesn't have to be the case
+        context.addServletMappingDecoded( "/", "helloServlet");
+
+        //starting the tomcat server
+        tomcat.start();
+        //new thread which keeps the server running
+        new Thread(()->tomcat.getServer().await()).start();
+    }
+
 }
